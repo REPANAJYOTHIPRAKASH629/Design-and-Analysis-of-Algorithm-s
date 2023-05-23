d68d31b3751316f3fab6de79e73e54ada1ab0651
REPANA JYOTHI PRAKASH <98946604+REPANAJYOTHIPRAKASH629@users.noreply.github.com>
2023-05-23 10:50:48 +0530

    job_scheduling.c

--- job_scheduling.c
+++ job_scheduling.c
@@ -1,3 +1,52 @@
+Pseudo Code
+
+/*
+FUNCTION findMax(a[], n):
+    max = 0
+    FOR i = 0 to n-1:
+        IF a[i] > max:
+            max = a[i]
+    RETURN max
+FUNCTION setValues(a[], n):
+    FOR i = 0 to n-1:
+        a[i] = 0
+FUNCTION setProfit(a[], n, p):
+    FOR i = 0 to n-1:
+        IF a[i] == 0:
+            a[i] = p
+            BREAK
+FUNCTION main():
+    DECLARE profit[20], dl[20], ts[10]
+    DECLARE maxdl, p, n, temp, i, j
+    PRINT "Enter the total number of Jobs"
+    READ n
+    PRINT "Enter the Deadlines of Jobs"
+    FOR i = 0 to n-1:
+        READ dl[i]
+    PRINT "Enter the Profits of Jobs"
+    FOR i = 0 to n-1:
+        READ profit[i]
+    maxdl = findMax(dl, n)
+    setValues(ts, maxdl)
+    FOR i = n-1 DOWNTO 1:
+        FOR j = 0 to i-1:
+            IF profit[j] < profit[j+1]:
+                SWAP profit[j] with profit[j+1]
+                SWAP dl[j] with dl[j+1]
+    FOR i = 0 to n-1:
+        IF ts[dl[i]-1] == 0:
+            ts[dl[i]-1] = profit[i]
+        ELSE:
+            setProfit(ts, dl[i]-1, profit[i])
+    p = 0
+    FOR i = 0 to maxdl-1:
+        p = p + ts[i]
+    PRINT "Maximum Profit is ", p
+END FUNCTION
+*/
+
+
+
 #include<stdio.h>
 
 int findmax(int a[],int n)
